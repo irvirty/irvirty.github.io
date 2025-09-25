1c24b86e64c4392f87f82bd58f6f29e243f0d136
irvirty <irvirty@gmail.com>
2025-09-25 16:34:08 +0300

    update

---
+++ data/gb3JsonVar.js
@@ -0,0 +1,14 @@
+
+var gb3JsonVar = 
+
+[
+    {
+        "text": "test text , test link: https:\/\/example.com\/",
+        "text2": "",
+        "text3": "",
+        "url": "",
+        "tag": "#gb3",
+        "time": 1758795639,
+        "rightFooter": ""
+    }
+]
---
+++ data/guestbook3JsonVar.js
@@ -0,0 +1,23 @@
+
+var guestbook3JsonVar = 
+
+[
+    {
+        "text": "test name\r\n---\r\nTest text\r\ntest text",
+        "text2": "",
+        "text3": "",
+        "url": "",
+        "tag": "#guestbook3",
+        "time": 1758806782,
+        "rightFooter": ""
+    },
+    {
+        "text": "3 test text , test link: https:\/\/example.com\/",
+        "text2": "",
+        "text3": "",
+        "url": "",
+        "tag": "#guestbook3",
+        "time": 1758795639,
+        "rightFooter": ""
+    }
+]

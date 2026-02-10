5018a3a04037d7b4ec0df5ad6564eaaf93b55050
irvirty <irvirty@gmail.com>
2026-02-10 14:49:48 +0200

    update

--- js/main.js
+++ js/main.js
@@ -378,7 +378,7 @@ document.getElementById("footer").innerHTML = `
 <a id="fSettings" class="brand inlineBlock padding" title="Settings" href="${confD}pages/settings/">Settings</a>
 <span class="op gray">|</span>
 
-<a class="brand inlineBlock padding" title="Social network" href="https://bsky.app/profile/${conf["confUsername"]}.pages.dev">Bluesky</a>
+<a class="brand inlineBlock padding" title="Social network" href="https://nostter.app/npub17ha3jmkrz767kgda656ql6qqquzvm0ath2872mxaa6547w4gcvys8eu8th">Nostter</a>
 <span class="op gray">|</span>
 
 <a class="brand inlineBlock padding" title="Source code (repository)" href="https://github.com/${conf["confUsername"]}/${conf["confWebsiteUrl"]}">Source Code</a>
